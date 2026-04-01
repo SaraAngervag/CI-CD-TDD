356f3192e4c97e0725b0fc6a2b02c4c427e122a1
anger <saraangervag@gmail.com>
2026-04-01 15:23:36 +0200

    UserTest

--- src/test/java/userTest/UserTest.java
+++ src/test/java/userTest/UserTest.java
@@ -8,25 +8,27 @@ public class UserTest {
 
     @Test
     public void testUserName() {
-        User user = new User("Staffan", "password");
-    String expected = "Staffan";
-    String actual = user.getUserName();
+        User user = new User("Anna", "password");
+        String expected = "Anna";
+        String actual = user.getUserName();
 
-    assertEquals(expected, actual);
-}
+        assertEquals(expected, actual);
+    }
+
     @Test
     public void testPassword() {
-        User user = new User("Staffan", "password");
+        User user = new User("Anna", "password");
         String expected = "password";
         String actual = user.getPassword();
 
         assertEquals(expected, actual);
     }
+
     @Test
     public void testSetUserName() {
-        User user = new User("Staffan", "password");
-      user.setUserName("Alan");
-        String expected = "Alan";
+        User user = new User("Anna", "password");
+        user.setUserName("Klara");
+        String expected = "Klara";
         String actual = user.getUserName();
 
         assertEquals(expected, actual);
